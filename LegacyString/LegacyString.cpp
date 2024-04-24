c27e2cd1f3b53973fe7410828b32232b0fda66eb
Xabartshik <danoshlakov12@gmail.com>
2024-04-24 15:07:05 +0900

    Добавьте файлы проекта.

---
+++ LegacyString/LegacyString.cpp
@@ -0,0 +1,13 @@
+﻿// LegacyString.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "LegacyString.h"
+
+using namespace std;
+
+int main()
+{
+	LegacyString::test();
+}
+
